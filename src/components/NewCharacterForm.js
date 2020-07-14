b1f7012061370cff7af8237e608af81167c62c51
Daniel Merys <dkmerys@gmail.com>
2020-07-14 13:30:03 -0700

    add 'add character' to database functionality

--- src/components/NewCharacterForm.js
+++ src/components/NewCharacterForm.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
+import { useFirestore } from 'react-redux-firebase';
 
-function NewCharacterForm(){
+function NewCharacterForm(props){
+
+  const firestore = useFirestore();
+
+  function addCharacterToFirestore(event) {
+    event.preventDefault();
+    // props.onNewCharacterCreation();
+    return firestore.collection('characters').add(
+      {
+        characterName: event.target.characterName.value,
+      }
+    );
+  }
   return(
-    <h1>testing</h1>
-  )
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={addCharacterToFirestore}
+        buttonText="Add Character" />
+    </React.Fragment>
+  );
 }
+
+NewCharacterForm.propTypes = {
+  onNewCharacterCreation: PropTypes.func
+}
+
 export default NewCharacterForm
